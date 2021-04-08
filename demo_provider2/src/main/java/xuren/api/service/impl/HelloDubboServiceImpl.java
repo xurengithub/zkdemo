5eb3044e0fd43788a153ed04550219666ad36919
xuren <1300360509@qq.com>
2021-04-08 11:14:48 +0800

    dubbo多提供者

---
+++ demo_provider2/src/main/java/xuren/api/service/impl/HelloDubboServiceImpl.java
@@ -0,0 +1,13 @@
+package xuren.api.service.impl;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.xuren.api.service.HelloDubboService;
+import org.springframework.stereotype.Component;
+
+@Service
+@Component
+public class HelloDubboServiceImpl implements HelloDubboService {
+    public String sayHello(String name) {
+        return "欢迎server2" + name + "使用Dubbo。";
+    }
+}
